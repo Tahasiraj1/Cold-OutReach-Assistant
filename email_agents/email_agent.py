743a9ccd56433280307131cf74423fb8eebedfc0
Tahasiraj1 <tahasiraj242@gmail.com>
2025-07-10 23:13:15 +0500

    Refactor email agent initialization by introducing a new utility function to retrieve the Gemini model. Update agent configurations to include handoff descriptions and modify the Email Assistant's instructions to incorporate a recommended prompt prefix.

--- email_agents/email_agent.py
+++ email_agents/email_agent.py
@@ -1,28 +1,18 @@
 from .instructions import COMPOSER_INSTRUCTIONS, EMAIL_ASSISTANT_INSTRUCTIONS, DRAFTER_INSTRUCTIONS
-from agents import Agent, AsyncOpenAI, set_tracing_disabled, OpenAIChatCompletionsModel
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
 from tools.process_pipeline import process_emails_pipeline
 from tools.compose_pipeline import compose_email_pipeline
 from tools.draft_pipeline import draft_new_email_pipeline
 from tools.draft_pipeline import draft_new_email_pipeline
-import os
+from utils.get_gemini_model import get_gemini_model
+from agents import Agent
 
-set_tracing_disabled(disabled=True)
-
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-
-provider = AsyncOpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-)
-
-model = OpenAIChatCompletionsModel(
-    model='gemini-2.5-flash-preview-05-20',
-    openai_client=provider,
-)
+model = get_gemini_model()
 
 drafter_agent = Agent(
     name="Drafter Agent",
     instructions=DRAFTER_INSTRUCTIONS,
+    handoff_description='You are a professional Email Drafter tasked with automating Gmail inbox management. Your objective is to efficiently draft new emails using the draft_new_email_pipeline function.',
     model=model,
     tools=[draft_new_email_pipeline],
 )
@@ -30,13 +20,14 @@ drafter_agent = Agent(
 composer_agent = Agent(
     name="Composer Agent",
     instructions=COMPOSER_INSTRUCTIONS,
+    handoff_description='You are a professional Email Composer tasked with automating Gmail inbox management. Your objective is to efficiently compose emails using the compose_email_pipeline function.',
     model=model,
     tools=[compose_email_pipeline],
 )
 
 email_assistant = Agent(
     name="Email Assistant",
-    instructions=EMAIL_ASSISTANT_INSTRUCTIONS,
+    instructions=f"{RECOMMENDED_PROMPT_PREFIX}\n\n{EMAIL_ASSISTANT_INSTRUCTIONS}",
     model=model,
     tools=[process_emails_pipeline],
     handoffs=[composer_agent, drafter_agent],
